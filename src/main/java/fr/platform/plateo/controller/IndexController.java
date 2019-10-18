d9ab93afa278511120ff50e460047162a2301320
sBerger <stephone13@gmail.com>
2019-10-18 10:29:57 +0200

    Version Test thymeleaf FormPro

--- src/main/java/fr/platform/plateo/controller/IndexController.java
+++ src/main/java/fr/platform/plateo/controller/IndexController.java
@@ -2,13 +2,21 @@ package fr.platform.plateo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
-	
+
 	@GetMapping("/")
 	public String index() {
-		
+
 		return "index";
 	}
+
+	@GetMapping("/pro/proForm")
+	public String proForm() {
+		
+		return "proForm";
+	}
+
 }
