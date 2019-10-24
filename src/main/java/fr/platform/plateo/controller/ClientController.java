892ac295a1171ccf336ae508bba90b02f29fefe8
Fouzi Benmakhlouf <popopolo@gmail.com>
2019-10-24 12:06:46 +0200

    remis packages dans les controllers

--- src/main/java/fr/platform/plateo/controller/ClientController.java
+++ src/main/java/fr/platform/plateo/controller/ClientController.java
@@ -1,3 +1,4 @@
+package fr.platform.plateo.controller;
 
 import java.io.IOException;
 import java.util.Date;
--- src/main/java/fr/platform/plateo/controller/proController.java
+++ src/main/java/fr/platform/plateo/controller/proController.java
@@ -1,4 +1,4 @@
-
+package fr.platform.plateo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
