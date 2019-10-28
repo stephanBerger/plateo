0ce0afcf2bf00cd362e667e8d9eb3b553bf94982
sBerger <stephone13@gmail.com>
2019-10-28 16:47:35 +0100

    Formulaire Pro + controller ok BD creer mais n'enregistre pas

--- src/main/java/fr/platform/plateo/controller/proController.java
+++ src/main/java/fr/platform/plateo/controller/proController.java
@@ -1,22 +1,62 @@
 package fr.platform.plateo.controller;
 
+import java.io.IOException;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.platform.plateo.entity.Pro;
+import fr.platform.plateo.service.ProRepository;
+import fr.platform.plateo.service.ProService;
 
 @Controller
 @RequestMapping("/pro")
 public class proController {
 	
+	
+	
+	@Autowired
+	private ProService proService;
+
+	@PostMapping("/create")
+	public String addPro(@Valid Pro pro, BindingResult result, Model model) throws IOException {
+
+		if (result.hasErrors()) {
+			System.out.println(result.toString());
+			return "pro/proForm";
+		}
+		BCryptPasswordEncoder decrypt = new BCryptPasswordEncoder(4);
+		String password = decrypt.encode(pro.getMot_passe_pro());
+
+		proService.create(pro);
+
+		return "index";
+
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@GetMapping("/proForm")
 	public String proForm() {
 		return "pro/proForm";
 	}
-	
+
 	@GetMapping("/dashboardPro")
 	public String dashboardPro() {
-		
-		return "dashboardPro";
+
+		return "pro/dashboardPro";
 	}
 
 }
