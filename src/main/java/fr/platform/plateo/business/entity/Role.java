6abaa8fc59212d087aa73a01d8d38eb327f4be7e
Fouzi Benmakhlouf <popopolo@gmail.com>
2019-10-30 14:35:32 +0100

    Role fini(?) et ajout du package config avec la classe WebSecurityConfig(non finie)

--- src/main/java/fr/platform/plateo/business/entity/Role.java
+++ src/main/java/fr/platform/plateo/business/entity/Role.java
@@ -1,15 +1,15 @@
 package fr.platform.plateo.business.entity;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.springframework.security.core.GrantedAuthority;
 
@@ -17,42 +17,48 @@ import org.springframework.security.core.GrantedAuthority;
  * 
  */
 @Entity
-@Table(name="roles")
-public class Role implements GrantedAuthority {
+@Table(name = "role")
+public class Role implements GrantedAuthority, Serializable {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -2301537539869858022L;
 
-    /**
-     * 
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+	/**
+	 * 
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    /**
-     * 
-     */
-    private String name;
-    
-    @ManyToMany(mappedBy = "roles")
-    private Collection<Pro> pros;
-    
-    @ManyToMany
-    @JoinTable(
-    		name = "roles_privileges",
-    		joinColumns = @JoinColumn(
-    				name = "role_id", referencedColumnName = "id"),
-    		inverseJoinColumns = @JoinColumn(
-    				name = "privilege_id", referencedColumnName = "id"))
-    private Collection<Privilege> privileges;
+	/**
+	 * 
+	 */
+	private String name;
+
+	//@ManyToMany(mappedBy = "role")
+	//private Collection<Pro> pros;
+
+	//@ManyToMany(mappedBy = "role")
+	//private Collection<Client> clients;
 
 	@Override
+	@Transient
 	public String getAuthority() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.getName();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
 	}
 
 }
---
+++ src/main/java/fr/platform/plateo/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package fr.platform.plateo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest().anonymous()
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+        
+    }
+	
+}
