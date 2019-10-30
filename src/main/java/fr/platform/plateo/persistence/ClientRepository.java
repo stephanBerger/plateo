54c894b9657a310f7f0842c88db07b88c26f7fef
livman <harich1@gmail.com>
2019-10-30 15:03:57 +0100

    rajout annotation sur ClientRepository.java

--- src/main/java/fr/platform/plateo/persistence/ClientRepository.java
+++ src/main/java/fr/platform/plateo/persistence/ClientRepository.java
@@ -1,12 +1,14 @@
 package fr.platform.plateo.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.platform.plateo.business.entity.Client;
 
 /**
  * 
  */
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
     Client findOneByUsername( final String Username );
 
