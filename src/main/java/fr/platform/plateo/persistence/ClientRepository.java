33e5577a360512294c748aaf0cb4d182e75782dd
Peer Mohummad <riyaz.peer@hotmail.fr>
2019-10-30 16:22:21 +0100

    erreur repertoire public

--- src/main/java/fr/platform/plateo/persistence/ClientRepository.java
+++ src/main/java/fr/platform/plateo/persistence/ClientRepository.java
@@ -10,6 +10,6 @@ import fr.platform.plateo.business.entity.Client;
  */
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
-    Client findOneByUsername( final String Username );
+    //Client findOneByUsername( final String Username );
 
 }
