2d218224e1a0e16327823f41b39a43d41c369628
Fouzi Benmakhlouf <popopolo@gmail.com>
2019-10-30 14:51:09 +0100

    Liste des ressources et accès selon roles dans WebSecurityConfig

--- src/main/java/fr/platform/plateo/config/WebSecurityConfig.java
+++ src/main/java/fr/platform/plateo/config/WebSecurityConfig.java
@@ -7,18 +7,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	String[] resources = new String[] {
+			"/include/**","/css/**","/icons/**","/IMG/**","/js/**",
+			"/layer/**","/resources/","/static/**","/webjars/**"
+	};
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
-                //HTTP Basic authentication
-                .httpBasic()
-                .and()
-                .authorizeRequests()
-                .anyRequest().anonymous()
-                .and()
-                .csrf().disable()
-                .formLogin().disable();
+    	http.authorizeRequests()
+    	.antMatchers(resources).permitAll()
+    	.antMatchers("/","/new/**").permitAll()
+        .antMatchers("/particulier*").hasRole("CLIENT")
+        .anyRequest()
+        .authenticated()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/pro*").hasRole("PRO")
+        .anyRequest()
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        // login ok route vers index du site
+        .defaultSuccessUrl("/")
+        .failureUrl("/login?error=true")
+        .usernameParameter("client_email_address")
+        .passwordParameter("client_password")
+        .and()
+        .logout()
+        .permitAll()
+        .logoutSuccessUrl("/");
         
     }
 	
