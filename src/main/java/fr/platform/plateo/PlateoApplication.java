8b1a52801511e835d356343890342e4cafd4d077
Fouzi Benmakhlouf <popopolo@gmail.com>
2019-10-30 13:28:36 +0100

    debut Role at ajout componentScans

--- src/main/java/fr/platform/plateo/PlateoApplication.java
+++ src/main/java/fr/platform/plateo/PlateoApplication.java
@@ -3,15 +3,15 @@ package fr.platform.plateo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @SpringBootApplication
-@ComponentScan({ "fr.platform.plateo.config"})
-@ComponentScan({ "fr.platform.plateo.controller"})
-@ComponentScan({ "fr.platform.plateo.entity"})
-@ComponentScan({ " fr.platform.plateo.service"})
-@ComponentScan({ "fr.platform.plateo.util"})
-
+@ComponentScan({ "fr.platform.plateo.presentation"})
+@ComponentScan({ "fr.platform.plateo.business.entity"})
+@ComponentScan({ "fr.platform.plateo.business.service"})
+@ComponentScan({ "fr.platform.plateo.persistence"})
+@EnableWebSecurity
 public class PlateoApplication {
 
 	public static void main(String[] args) {
