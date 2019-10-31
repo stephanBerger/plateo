6b176a1b46b280451f7a9dbf22976c7450f9454c
gregory-bonvalot <talock69@hotmail.com>
2019-10-31 11:07:37 +0100

    poule

--- src/main/java/fr/platform/plateo/config/SecurityConfig.java
+++ src/main/java/fr/platform/plateo/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig {
 	
 	static String[] resources = new String[] {
-			"/include/**","/css/**","/icons/**","/IMG/**","/js/**",
+			"/include/**","/css/**","/icons/**","/img/**","/js/**",
 			"/layer/**","/resources/","/static/**","/webjars/**"
 	};
 	
