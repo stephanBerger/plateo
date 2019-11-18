fedc328e7107f0d6a6168bb84bf3d25437d9e716
sBerger <stephone13@gmail.com>
2019-11-18 21:17:59 +0100

    popover sur liste profession pour afficher la description des corps d'états

--- src/main/java/fr/platform/plateo/business/entity/Profession.java
+++ src/main/java/fr/platform/plateo/business/entity/Profession.java
@@ -12,67 +12,78 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table( name = "profession" )
+@Table(name = "profession")
 public class Profession implements Serializable {
 
-    private static final long serialVersionUID = -2301537539869858022L;
+	private static final long serialVersionUID = -2301537539869858022L;
 
-    /**
-     *
-     */
-    @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
-    private Integer           id;
+	/**
+	 *
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column( name = "profession_name" )
-    private String            name;
+	@Column(name = "profession_name")
+	private String name;
 
-    @Column( name = "profession_code", length = 3, unique = true )
-    private String            code;
+	@Column(name = "profession_code", length = 3, unique = true)
+	private String code;
 
-    @Column( name = "profession_enabled" )
-    private Boolean           enabled;
+	@Column(name = "description")
+	private String description;
 
-    @OneToMany( mappedBy = "profession" )
-    private List<Service>     services;
+	@Column(name = "profession_enabled")
+	private Boolean enabled;
 
-    public Integer getId() {
-        return this.id;
-    }
+	@OneToMany(mappedBy = "profession")
+	private List<Service> services;
 
-    public void setId( Integer id ) {
-        this.id = id;
-    }
+	public String getDescription() {
+		return this.description;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setName( String name ) {
-        this.name = name;
-    }
+	public Integer getId() {
+		return this.id;
+	}
 
-    public String getCode() {
-        return this.code;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setCode( String code ) {
-        this.code = code;
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public Boolean getEnabled() {
-        return this.enabled;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setEnabled( Boolean enabled ) {
-        this.enabled = enabled;
-    }
+	public String getCode() {
+		return this.code;
+	}
 
-    public List<Service> getServices() {
-        return this.services;
-    }
+	public void setCode(String code) {
+		this.code = code;
+	}
 
-    public void setServices( List<Service> services ) {
-        this.services = services;
-    }
+	public Boolean getEnabled() {
+		return this.enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public List<Service> getServices() {
+		return this.services;
+	}
+
+	public void setServices(List<Service> services) {
+		this.services = services;
+	}
 }
