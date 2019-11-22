536c62d1dc29706159f7d3c6a73c1ae1dc62a0ea
sBerger <stephone13@gmail.com>
2019-11-22 14:37:21 +0100

    stephan modif

--- src/main/java/fr/platform/plateo/business/service/BusinessProcessModelService.java
+++ src/main/java/fr/platform/plateo/business/service/BusinessProcessModelService.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import org.activiti.engine.FormService;
 import org.activiti.engine.HistoryService;
-import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.form.FormProperty;
@@ -16,24 +15,18 @@ import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import fr.platform.plateo.business.entity.EstimateHasService;
-import fr.platform.plateo.persistence.EstimateHasServiceRepository;
-import fr.platform.plateo.persistence.EstimateRepository;
+import fr.platform.plateo.business.entity.Service;
 import fr.platform.plateo.persistence.ProfessionRepository;
 import fr.platform.plateo.persistence.ServiceRepository;
 
-@Service
+@org.springframework.stereotype.Service
 public class BusinessProcessModelService {
 
 	private static final String PROCESS_NAME_PREFIX = "makeEstimate-";
 
-	@Autowired
-	private Logger LOGGER;
-	
-	@Autowired
-	private ProcessEngine processEngine;
+	private static final Logger LOGGER = LoggerFactory.getLogger(BusinessProcessModelService.class);
 
 	@Autowired
 	private RuntimeService runtimeService;
@@ -58,61 +51,47 @@ public class BusinessProcessModelService {
 
 	@Autowired
 	private EstimateHasServService estimateHSService;
-	
-	
-	
-	public Task startProcess(Integer assigneeId, Integer professionId,
-			Integer serviceId) {
-		String professionCode = this.professionRepo.getOne(professionId)
-				.getCode();
+
+	public Task startProcess(Integer assigneeId, Integer professionId, Integer serviceId) {
+		String professionCode = this.professionRepo.getOne(professionId).getCode();
 		String serviceCode = this.serviceRepo.getOne(serviceId).getCode();
-		this.LOGGER.info(
-				"STARTING PROCESS {} {} {} !", assigneeId, professionCode,
-				serviceCode);
+		BusinessProcessModelService.LOGGER.info("STARTING PROCESS {} {} {} !", assigneeId, professionCode, serviceCode);
 		Map<String, Object> vars = new HashMap<>();
 		vars.put("account", this.clientService.findId(assigneeId).get());
-		ProcessInstance pi = this.runtimeService
-				.startProcessInstanceByKey(
-						BusinessProcessModelService.PROCESS_NAME_PREFIX
-								+ professionCode + "-" + serviceCode,
-						vars);
-		// TODO: Créer le devis avec EstimateHasService et data contenant le process
-		// instance Id.
-		Task task = this.getTaskByProcessInstanceIdAndAssigneeId(
-				assigneeId, pi.getProcessInstanceId());
+		ProcessInstance pi = this.runtimeService.startProcessInstanceByKey(
+				BusinessProcessModelService.PROCESS_NAME_PREFIX + professionCode + "-" + serviceCode, vars);
+		Task task = this.getTaskByProcessInstanceIdAndAssigneeId(assigneeId, pi.getProcessInstanceId());
 		return task;
-		
 	}
 
-	public Task getTaskByProcessInstanceIdAndAssigneeId(Integer assigneeId,
-			String processInstanceId) {
-		return this.taskService.createTaskQuery()
-				.processInstanceId(processInstanceId)
-				.taskAssignee(assigneeId.toString()).singleResult();
-	}
-
-	public void save(String taskId, Map<String, Object> params) {
-		this.LOGGER.info("PARAMS: {}", params);
-		this.taskService.complete(taskId, params);
-	}
-
-	public List<FormProperty> getFormData(String taskId) {
-		return this.processEngine.getFormService().getTaskFormData(taskId)
-				.getFormProperties();
-	}
 	public Map<Service, Map<String, Object>> getDataForAssigneeAndProcess(Integer assigneeId, Integer estimateId) {
 		Map<Service, Map<String, Object>> services = new HashMap<>();
-		
 		List<EstimateHasService> ehsList = this.estimateHSService.readByEstimateId(estimateId);
 		for (EstimateHasService ehs : ehsList) {
 			HistoricProcessInstance pi = this.historyService.createHistoricProcessInstanceQuery()
 					.includeProcessVariables().processInstanceId(ehs.getProcessid()).singleResult();
 			Map<String, Object> props = pi.getProcessVariables();
+			
 			// Retirer les variables qui ne concernent pas la prestation du devis.
 			props.remove("_csrf");
 			props.remove("account");
 			services.put(ehs.getService(), props);
 		}
+		
 		return services;
+	}
+
+	public Task getTaskByProcessInstanceIdAndAssigneeId(Integer assigneeId, String processInstanceId) {
+		return this.taskService.createTaskQuery().processInstanceId(processInstanceId)
+				.taskAssignee(assigneeId.toString()).singleResult();
+	}
+
+	public void save(String taskId, Map<String, Object> params) {
+//		BusinessProcessModelService.LOGGER.info("PARAMS: {}", params);
+		this.taskService.complete(taskId, params);
+	}
+
+	public List<FormProperty> getFormData(String taskId) {
+		return this.formService.getTaskFormData(taskId).getFormProperties();
 	}
 }
